1c7d804d010bf9f2a7c796b38338bc6b2ad5a5f2
hamritha04 <hamrithav3@gmail.com>
2026-09-21 23:16:00 +0530

    Update Status message

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 app.get('/status', (req, res) => {
     res.json({
-        status: 'Application is running successfully'
+        status: 'Node.js application deployed through CI/CD'
     });
 });
 
